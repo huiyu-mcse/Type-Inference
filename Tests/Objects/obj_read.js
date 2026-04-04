de2f8707400e02dae68896da41f35337d1d89f53
huiyu-mcse <hui@suface-laptop.localdomain>
2026-04-04 14:56:24 +0100

    Some test results added

--- Tests/Objects/obj_read.js
+++ Tests/Objects/obj_read.js
@@ -1,18 +1,30 @@
-var obj = {};
+
+/**
+ * 
+ * var obj = {};
 obj.count = 0;
 var count = obj.count;
 var next  = count + 1;
 obj.count = next;
+ */
+
 
 var person = {};
-person.name = "Ada";
+//person.name = "Ada";
 person.age = 36;
 
+//var n = person.name;
+
+var a = person.age;
+
+var b = a;
+
+
 function greet() {
-  var n = person.name;
-  var a = person.age;
-  var combined = a + 1;
+  
+  //var a = person.age;
+  //var combined = a + 1;
 }
 
-
 //有问题
+// TODO: Last part of the logic left, when the types are not calculated deterministiclly, we have to decide the final type....
