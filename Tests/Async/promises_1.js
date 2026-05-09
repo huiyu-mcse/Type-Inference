8b50ade2b5502d2f0f7271593dc14abc68163b10
José Lopes <josersl2003@gmail.com>
2026-05-09 15:36:47 +0100

    tests(Promises): Add three test cases of Promises.

---
+++ Tests/Async/promises_1.js
@@ -0,0 +1,14 @@
+// Xp <= Promise(X_res_arg, X_rej_arg), number <= X_res_arg
+var p = new Promise(function (res, rej) {
+  res(5);
+});
+
+var z;
+
+// X_res_arg <= X_z
+p.then(function (x) {
+  z = x;
+});
+
+// (Xp <= Promise(X_res_arg, X_rej_arg), number <= X_res_arg, X_res_arg <= X_z);
+// (Xp <= Promise(number), (X_z = number));
